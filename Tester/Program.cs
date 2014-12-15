9db1663b3f6232e9d75905d1104a25da970c3f04
Kristof Mattei <delegatevoid@gmail.com>
2014-12-15 15:22:27 +0100

    Added tester, to see how we're going to construct the reading and writing of properties

---
+++ Tester/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Tester
+{
+    using System;
+    using System.Drawing;
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var bitmap = new Bitmap(@"c:\temp\photo.jpg");
+
+            foreach (var item in bitmap.PropertyItems)
+            {
+                Console.WriteLine("Id: {0}, Type: {1}, Value: {2}", item.Id, item.Type, item.Value);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
